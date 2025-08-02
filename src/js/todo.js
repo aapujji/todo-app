14d7775c3d8592a605f43d8388a1adb0f3c55eff
Aanandita <aanandita.pujji@gmail.com>
2025-08-01 23:05:04 -0400

    add due date and priority properties to todo factory function

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,10 +1,12 @@
-const todo = (title, completed = false) => {
+const todo = (title, completed = false, dueDate = false, priority = 3) => {
     const id = crypto.randomUUID();
 
     let todo = {};
     todo.id = id;
     todo.title = title;
     todo.completed = completed;
+    todo.dueDate = dueDate;
+    todo.priority = priority;
 
     return todo;
 }
