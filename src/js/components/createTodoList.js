b27dea3afc86c6625327fb34cf833fed744eedbd
Aanandita <aanandita.pujji@gmail.com>
2025-08-06 11:14:14 -0400

    Deployment commit

--- src/js/components/createTodoList.js
+++
@@ -1,9 +0,0 @@
-import { getProjectTodos } from "../modules/data";
-import { updateTodoList } from "../dom";
-
-const createTodoList = (project) => {
-    const projectTodos = getProjectTodos(project);
-    updateTodoList(project, projectTodos);
-}
-
-export default createTodoList;
